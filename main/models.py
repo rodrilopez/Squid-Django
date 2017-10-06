e25dbd9f3573d97fd7ac971f3f7668ec594a9491
lmbringas <lm.bringas98@gmil.com>
2017-10-06 10:31:11 -0300

    fix

--- main/models.py
+++ main/models.py
@@ -2,24 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
 # Create your models here.
 
 class Permission(models.Model):
     name = models.ChartField(max_length=32)
     description = models.ChartField(max_length=64)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
 class Category(models.Model):
     name = models.ChartField(max_length=32)
     description = models.ChartField(max_length=64)
+
 
 class Group(models.Model):
     name = models.ChartField(max_length=32)
     description = models.ChartField(max_length=64)
     red = models.ChartField(max_length=64)
     subnet = models.ChartField(max_length=128)
+    persmission = GenericRelation(persmission)
 
 class Person(models.Modal):
     name = models.ChartField(max_length=32)
     description = models.ChartField(max_length=64)
     ip = models.ChartField(max_length=64)
+    persmission = GenericRelation(persmission)
