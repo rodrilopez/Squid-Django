397fbd605960351588eab15f4707f964db64d44e
lmbringas <lm.bringas98@gmil.com>
2017-10-09 08:36:38 -0300

    .gitignore is now working

--- main/models.py
+++ main/models.py
@@ -11,11 +11,14 @@ from django.contrib.contenttypes.fields import GenericRelation
 class Permission(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=64)
+    is_white_list = models.BooleanField()
+    persmission = models.TextField()
 
 
 class Category(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=64)
+    persmission = models.TextField()
 
 
 class Group(models.Model):
@@ -23,10 +26,23 @@ class Group(models.Model):
     description = models.CharField(max_length=64)
     red = models.CharField(max_length=64)
     subnet = models.CharField(max_length=128)
-    persmission = GenericRelation(Permission)
+
 
 class Person(models.Model):
     name = models.CharField(max_length=32)
     description = models.CharField(max_length=64)
     ip = models.CharField(max_length=64)
-    permission = GenericRelation(Permission)
+
+
+class PermissionGroup(models.Model):
+    persmission = models.ForeignKey(Permission)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class CategoryGroup(models.Model):
+    category = models.ForeignKey(Category)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
